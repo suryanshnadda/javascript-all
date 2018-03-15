e0b4f15fa0705642daabd6463da9720a45b9241d
suryanshnadda <suryanshnadda@gmail.com>
2018-03-16 01:19:10 +0530

    test vcs embed

--- START/7operators.js
+++ START/7operators.js
@@ -1,41 +1,36 @@
-function fun()
-{
-var a=("1" +2);
+function fun() {
+    var a = ("1" + 2);
 
-document.getElementById("one").innerHTML = a;
+    document.getElementById("one").innerHTML = a;
 
-a=("1"-"2")
-document.getElementById("two").innerHTML = a;
+    a = ("1" - "2")
+    document.getElementById("two").innerHTML = a;
 
-a=("6"/"2")
-document.getElementById("three").innerHTML = a;
+    a = ("6" / "2")
+    document.getElementById("three").innerHTML = a;
 
 
 //here is hack only + operator do not well for strings and - and / works good with string numberic values also
 
-a=("3"-1);
-document.getElementById("four").innerHTML = a;
+    a = ("3" - 1);
+    document.getElementById("four").innerHTML = a;
 
-a=a++;
-document.getElementById("five").innerHTML = a;
+    a = a++;
+    document.getElementById("five").innerHTML = a;
 
 //a-- goes the same
+//git
+    bhjh
+    a * * 3;
+    document.getElementById("six").innerHTML = a;
 
-a**3;
-document.getElementById("six").innerHTML = a;
+    a = a % 2;
+    document.getElementById("seven").innerHTML = a;
 
-a=a%2;
-document.getElementById("seven").innerHTML = a;
-
-a="nody" +"india";
-document.getElementById("eight").innerHTML = a;
-
-
-
-
-
-
+    a = "nody" + "india";
+    document.getElementById("eight").innerHTML = a;
 
 
 }
+
 fun();
