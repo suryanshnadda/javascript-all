102606df31c75f4accf1b8f46975707e243b625a
suryanshnadda <suryanshnadda@gmail.com>
2018-03-15 19:24:30 +0530

    day2 evening
    2nd phase of the moment

---
+++ START/9alertprompt.js
@@ -0,0 +1,13 @@
+"use strict"
+alert("this is a simple alert! you can this  use this as a warning");
+
+let name=prompt(`please enter your name`,"nody");
+
+alert(`hello ${name}  welcome to codenody`);
+
+document.write(`${name}`);
+
+
+let stu =confirm("are you a student");
+
+document.write(`<br> ${name} is  a student ${stu}`);
